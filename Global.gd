64350b7ab7e8ebb5b117bccd6502f83899190b03
T Gibson <taylornicolegibson@gmail.com>
2022-03-31 17:10:32 -0400

    progress

--- Global.gd
+++ Global.gd
@@ -4,8 +4,16 @@ var score = 0
 var timer = 0
 
 func _unhandled_input(event):
-	if event.is_action_pressed("quit"):
-		get_tree().quit()
+	if event.is_action_pressed("menu"):
+		var menu = get_node_or_null("root/Game/UI/Menu")
+		if menu != null:
+			var p = get_tree().paused
+			if p:
+				menu.hide()
+				get_tree().paused = false
+			else:
+				menu.show()
+				get_tree().paused = true
 
 func increase_score(s):
 	score += s
---
+++ UI/Menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _on_Save_pressed():
+	pass # Replace with function body.
+
+
+func _on_Load_pressed():
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
