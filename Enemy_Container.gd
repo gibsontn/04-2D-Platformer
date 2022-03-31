c85771d22c6be42b2f1b22378c82b3ba1e9c7714
T Gibson <taylornicolegibson@gmail.com>
2022-03-31 13:25:44 -0400

    progress

--- Enemy_Container.gd
+++ Enemy_Container.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 onready var Scorpion = load("res://Enemy/Enemy_Sprites/Scorpion.tscn")
+onready var Goblin = load("res://Enemy/Enemy_Sprites/Goblin.tscn")
 
 func _physics_process(_delta):
 	if not has_node("Scorpion"):
 		var scorpion = Scorpion.instance()
 		add_child(scorpion)
 		scorpion.name = 'Scorpion'
+	if not has_node("Goblin"):
+		var goblin = Goblin.instance()
+		add_child(goblin)
+		goblin.name = 'Goblin'
