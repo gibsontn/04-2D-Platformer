2aa8908d11a3ee1da5d8ff5ce72a18af3412f087
T Gibson <taylornicolegibson@gmail.com>
2022-03-31 22:36:50 -0400

    progress

--- Coins/Coin_Container.gd
+++ Coins/Coin_Container.gd
@@ -2,11 +2,9 @@ extends Node2D
 
 onready var Coin = load("res://Coins/Coin.tscn")
 
-func _ready():
-	pass
 
 func spawn(attr, p):
 	var coin = Coin.instance()
 	for a in attr:
 		coin[a] = attr[a]
-		coin.position = p
+	coin.position = p
--- StateMachine/Player_Container.gd
+++ StateMachine/Player_Container.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-var starting_position = Vector2(200,200)
+export var spawn_location = Vector2(200,200)
 onready var Player = load("res://Player/Player.tscn")
 
 
 func _physics_process(_delta):
 	var player = get_node_or_null("Player")
 	if player == null:
-		spawn(starting_position)
+		spawn(spawn_location)
 
 func spawn(p):
 	var player = Player.instance()
